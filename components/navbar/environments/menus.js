86b66912a460e8183cdd34175f01de13e7817907
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2022-06-11 22:02:34 +0700

    update url

--- components/navbar/environments/menus.js
+++ components/navbar/environments/menus.js
@@ -5,14 +5,21 @@ export default [
   {
     id: 'mainnet',
     title: 'Mainnet',
-    path: process.env.NEXT_PUBLIC_SITE_URL?.replace('testnet.', ''),
+    path: 'https://axelarscan.io',
     external: true,
     icon: <RiRadioButtonLine size={20} className="stroke-current" />,
   },
   {
     id: 'testnet',
     title: 'Testnet',
-    path: process.env.NEXT_PUBLIC_SITE_URL?.replace('staging.', '').replace('://', `://${process.env.NEXT_PUBLIC_ENVIRONMENT === 'mainnet' ? 'testnet.' : ''}`),
+    path: 'https://testnet.axelarscan.io',
+    external: true,
+    icon: <HiCode size={20} className="stroke-current" />,
+  },
+  {
+    id: 'testnet-2',
+    title: 'Testnet 2',
+    path: 'https://testnet-2.axelarscan.io',
     external: true,
     icon: <HiCode size={20} className="stroke-current" />,
   },
