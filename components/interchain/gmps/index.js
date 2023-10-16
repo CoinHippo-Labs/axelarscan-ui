a4b979a2daed3acb16d5650745274be92b15f93f
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-10-16 16:55:11 +0700

    chore: update display status

--- components/interchain/gmps/index.js
+++ components/interchain/gmps/index.js
@@ -472,8 +472,8 @@ export default () => {
                     },
                     {
                       id: 'execute',
-                      title: executed || is_executed ? 'Executed' : errored ? 'Error' : 'Execute',
-                      status: executed || is_executed ? 'success' : errored ? 'failed' : 'pending',
+                      title: (executed && (!executed.axelarTransactionHash || (executed.transactionHash && !error))) || is_executed ? 'Executed' : errored ? 'Error' : 'Execute',
+                      status: (executed && (!executed.axelarTransactionHash || (executed.transactionHash && !error))) || is_executed ? 'success' : errored ? 'failed' : 'pending',
                       data: executed || is_executed || error,
                       chain_data: executed?.axelarTransactionHash && !executed.transactionHash ? axelar_chain_data : destination_chain_data,
                     },
