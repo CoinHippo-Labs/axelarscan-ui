20690b800a3fec8930bd93661e9255ef601833bd
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2024-02-06 00:07:20 +0700

    feat: not copy when there is link inside

--- src/components/Copy.jsx
+++ src/components/Copy.jsx
@@ -11,21 +11,28 @@ export function Copy({ size = 18, value, onCopy, children, childrenClassName, cl
     return () => clearTimeout(timeout)
   }, [copied])
 
+  const handleCopy = () => {
+    setCopied(true)
+    if (onCopy) onCopy()
+  }
+  const isLinkInside = !!children?.props?.href
+
   return copied ?
     <div className={clsx('flex items-center gap-x-1 3xl:gap-x-2', children && 'min-w-max', childrenClassName)}>
       {children}
       <LuClipboardCheck size={size} className={clsx('3xl:w-6 3xl:h-6 min-w-fit text-green-500 dark:text-green-400', className)} />
     </div> :
-    <CopyToClipboard
-      text={value}
-      onCopy={() => {
-        setCopied(true)
-        if (onCopy) onCopy()
-      }}
-    >
-      <div className={clsx('flex items-center gap-x-1 3xl:gap-x-2', children && 'min-w-max', childrenClassName)}>
+    isLinkInside ?
+      <div className={clsx('min-w-max flex items-center gap-x-1 3xl:gap-x-2', childrenClassName)}>
         {children}
-        <LuClipboard size={size} className={clsx('3xl:w-6 3xl:h-6 min-w-fit cursor-pointer text-zinc-300 hover:text-zinc-400 dark:text-zinc-600 dark:hover:text-zinc-500', className)} />
-      </div>
-    </CopyToClipboard>
+        <CopyToClipboard text={value} onCopy={() => handleCopy()}>
+          <LuClipboard size={size} className={clsx('3xl:w-6 3xl:h-6 min-w-fit cursor-pointer text-zinc-300 hover:text-zinc-400 dark:text-zinc-600 dark:hover:text-zinc-500', className)} />
+        </CopyToClipboard>
+      </div> :
+      <CopyToClipboard text={value} onCopy={() => handleCopy()}>
+        <div className={clsx('flex items-center gap-x-1 3xl:gap-x-2', children && 'min-w-max', childrenClassName)}>
+          {children}
+          <LuClipboard size={size} className={clsx('3xl:w-6 3xl:h-6 min-w-fit cursor-pointer text-zinc-300 hover:text-zinc-400 dark:text-zinc-600 dark:hover:text-zinc-500', className)} />
+        </div>
+      </CopyToClipboard>
 }
