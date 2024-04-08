c27e6a7e1cc1f6abb6b7c6ff5295e368b5d4924c
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2024-04-08 21:39:03 +0700

    chore: move assets filter under chain

--- src/components/Interchain.jsx
+++ src/components/Interchain.jsx
@@ -78,10 +78,10 @@ function Filters() {
     { label: 'Source Chain', name: 'sourceChain', type: 'select', multiple: true, options: _.orderBy(toArray(chains).map((d, i) => ({ ...d, i })), ['deprecated', 'i'], ['desc', 'asc']).map(d => ({ value: d.id, title: d.name })) },
     { label: 'Destination Chain', name: 'destinationChain', type: 'select', multiple: true, options: _.orderBy(toArray(chains).map((d, i) => ({ ...d, i })), ['deprecated', 'i'], ['desc', 'asc']).map(d => ({ value: d.id, title: d.name })) },
     { label: 'From / To Chain', name: 'chain', type: 'select', multiple: true, options: _.orderBy(toArray(chains).map((d, i) => ({ ...d, i })), ['deprecated', 'i'], ['desc', 'asc']).map(d => ({ value: d.id, title: d.name })) },
-    { label: 'Contract', name: 'contractAddress' },
-    { label: 'Asset Type', name: 'assetType', type: 'select', options: _.concat({ title: 'Any' }, [{ value: 'gateway', title: 'Gateway Token' }, { value: 'its', title: 'ITS Token' }]) },
     { label: 'Asset', name: 'asset', type: 'select', multiple: true, options: toArray(_.concat(params.assetType !== 'its' && toArray(assets).map(d => ({ value: d.id, title: d.symbol })), params.assetType !== 'gateway' && toArray(itsAssets).map(d => ({ value: d.symbol, title: `${d.symbol} (ITS)` })))) },
     params.assetType === 'its' && { label: 'ITS Token Address', name: 'itsTokenAddress' },
+    { label: 'Contract', name: 'contractAddress' },
+    { label: 'Asset Type', name: 'assetType', type: 'select', options: _.concat({ title: 'Any' }, [{ value: 'gateway', title: 'Gateway Token' }, { value: 'its', title: 'ITS Token' }]) },
     { label: 'Time', name: 'time', type: 'datetimeRange' },
   ])
 
--- src/components/Transfers.jsx
+++ src/components/Transfers.jsx
@@ -61,11 +61,11 @@ function Filters() {
     { label: 'Source Chain', name: 'sourceChain', type: 'select', multiple: true, options: _.orderBy(toArray(chains).map((d, i) => ({ ...d, i })), ['deprecated', 'i'], ['desc', 'asc']).map(d => ({ value: d.id, title: d.name })) },
     { label: 'Destination Chain', name: 'destinationChain', type: 'select', multiple: true, options: _.orderBy(toArray(chains).map((d, i) => ({ ...d, i })), ['deprecated', 'i'], ['desc', 'asc']).map(d => ({ value: d.id, title: d.name })) },
     { label: 'From / To Chain', name: 'chain', type: 'select', multiple: true, options: _.orderBy(toArray(chains).map((d, i) => ({ ...d, i })), ['deprecated', 'i'], ['desc', 'asc']).map(d => ({ value: d.id, title: d.name })) },
+    { label: 'Asset', name: 'asset', type: 'select', multiple: true, options: toArray(assets).map(d => ({ value: d.id, title: d.symbol })) },
     { label: 'Type', name: 'type', type: 'select', options: _.concat({ title: 'Any' }, [{ value: 'deposit_address', title: 'Deposit Address' }, { value: 'send_token', title: 'Send Token' }, { value: 'wrap', title: 'Wrap' }, { value: 'unwrap', title: 'Unwrap' }, { value: 'erc20_transfer', title: 'ERC20 Transfer' }]) },
     { label: 'Status', name: 'status', type: 'select', options: _.concat({ title: 'Any' }, ['executed', 'failed'].map(d => ({ value: d, title: capitalize(d) }))) },
     { label: 'Sender', name: 'senderAddress' },
     { label: 'Recipient', name: 'recipientAddress' },
-    { label: 'Asset', name: 'asset', type: 'select', multiple: true, options: toArray(assets).map(d => ({ value: d.id, title: d.symbol })) },
     { label: 'Time', name: 'time', type: 'datetimeRange' },
     { label: 'Sort By', name: 'sortBy', type: 'select', options: _.concat({ title: 'Any' }, [{ value: 'time', title: 'Transfer Time' }, { value: 'value', title: 'Transfer Value' }]) },
   ]
