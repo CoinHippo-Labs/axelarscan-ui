33eb03dd52cea06ae1151e708f3e3e7dab8829ab
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2024-05-14 14:25:47 +0700

    chore: add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,7 +54,8 @@ const environments = [
   { name: 'mainnet', href: 'https://axelarscan.io' },
   { name: 'testnet', href: 'https://testnet.axelarscan.io' },
   { name: 'stagenet', href: 'https://stagenet.axelarscan.io' },
-].filter(d => d.name !== 'stagenet' || d.name === ENVIRONMENT)
+  { name: 'devnet', href: 'https://devnet.axelarscan.io' },
+].filter(d => !['stagenet', 'devnet'].includes(d.name) || d.name === ENVIRONMENT)
 
 function MobileNavLink({ href, children, className }) {
   const pathname = usePathname()
